5524f552f68304aaab9bb2020a2dcacb0385a8bb
wrp1002 <wes.hamster@gmail.com>
2021-07-09 19:11:06 -0400

    fancier prefs

--- shutupshortcutsprefs/SSCRootListController.h
+++ shutupshortcutsprefs/SSCRootListController.h
@@ -1,6 +1,8 @@
 #import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
 #import <Cephei/HBPreferences.h>
 #import <Cephei/HBRespringController.h>
 
 @interface SSCRootListController : PSListController
+@property (nonatomic, retain) NSMutableDictionary *savedSpecifiers;
 @end
--- shutupshortcutsprefs/SSCRootListController.m
+++ shutupshortcutsprefs/SSCRootListController.m
@@ -6,7 +6,40 @@
 	if (!_specifiers) {
 		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
 	}
+
+	NSArray *chosenIDs = @[@"kDisableAll", @"kDisableAllGroup"];
+	self.savedSpecifiers = (self.savedSpecifiers) ?: [[NSMutableDictionary alloc] init];
+	for(PSSpecifier *specifier in _specifiers) {
+		if([chosenIDs containsObject:[specifier propertyForKey:@"id"]])
+			[self.savedSpecifiers setObject:specifier forKey:[specifier propertyForKey:@"id"]];
+	}
+
 	return _specifiers;
+}
+
+-(void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
+	[super setPreferenceValue:value specifier:specifier];
+
+	NSString *key = [specifier propertyForKey:@"key"];
+	if([key isEqualToString:@"kEnabled"]) {
+		if([value boolValue])
+			[self insertContiguousSpecifiers:@[self.savedSpecifiers[@"kDisableAllGroup"], self.savedSpecifiers[@"kDisableAll"]] afterSpecifierID:@"kEnabled" animated:YES];
+		else
+			[self removeSpecifierID:@"kDisableAllGroup" animated:YES];
+	}
+}
+
+-(void)reloadSpecifiers {
+	[super reloadSpecifiers];
+
+	HBPreferences *prefs = [[HBPreferences alloc] initWithIdentifier:@"com.wrp1002.shutupshortcuts"];
+	if(![prefs boolForKey:@"kEnabled"])
+		[self removeSpecifierID:@"kDisableAllGroup" animated:NO];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+	[super viewWillAppear:animated];
+	[self reloadSpecifiers];
 }
 
 -(void)Respring {
